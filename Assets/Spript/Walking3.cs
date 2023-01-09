69702a02aee2057723bbe2c46bac72814ca7028e
Walter Alves <maximo.alvesdeoliveira@gmail.com>
2023-01-09 15:23:11 -0300

    Update 02

---
+++ Assets/Spript/Walking3.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking3 : MonoBehaviour
+{
+    public float JumpForce;
+    public float Speed;
+
+
+    public bool isJumping;
+    public bool doubleJump;
+
+    private Rigidbody2D rig;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+    
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * Speed;
+
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") )
+        {
+            if(isJumping == false)
+            {
+                rig.AddForce(new Vector2(0f , JumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+                
+            }
+            if(isJumping == true)
+            {
+                if(doubleJump)
+                {
+                    rig.AddForce(new Vector2(0f , JumpForce), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+                
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 7)
+        {
+            isJumping = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 7)
+        {
+            isJumping = true;
+        }
+    }
+}
